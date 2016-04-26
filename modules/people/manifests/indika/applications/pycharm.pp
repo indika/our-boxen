3dbdad9ef4d6cc680842a1db7a0cedab2dd2f1d6
Indika Piyasena <indika@hydrasqr.com>
2016-04-26 23:57:34 +1000

    cleaning up pycharm

--- modules/people/manifests/indika/applications/pycharm.pp
+++ modules/people/manifests/indika/applications/pycharm.pp
@@ -1,21 +1,8 @@
-class people::indika::applications::pycharm (
-  $version = '4.5.4',
-) {
+class people::indika::applications::pycharm {
 
+  package { 'pycharm': provider => 'brewcask' }
 
-  $pycharm_home = "/Users/${::boxen_user}/Library/Preferences/PyCharm40"
-
-
-  # https://download.jetbrains.com/python/pycharm-professional-2016.1.dmg
-  # https://download.jetbrains.com/python/pycharm-professional-4.5.3.dmg
-  # package { 'PyCharm':
-  #   provider => 'appdmg',
-
-  #   source   => "http://download.jetbrains.com/python/pycharm-professional-${version}.dmg",
-  #   owner    => 'indika',
-  #   group    => 'admin',
-  # }
-
+  $pycharm_home = "/Users/${::boxen_user}/Library/Preferences/PyCharm2016.1"
 
   notice("I need to backup my Pycharm keymaps and options once I am happy with it")
 
@@ -28,24 +15,31 @@ class people::indika::applications::pycharm (
   #   group    => 'staff',
   # }
 
-  # file { "${pycharm_home}/options":
-  #    ensure   => link,
-  #    target   => '/Users/indika/dev/config/pycharm/options',
-  #    owner    => 'indika',
-  #    group    => 'staff',
-  #    force    => 'true',
-  #    mode     => 700,
+  file { "${pycharm_home}/options":
+     ensure   => link,
+     target   => '/Users/indika/dev/config/pycharm/options',
+     owner    => 'indika',
+     group    => 'staff',
+     force    => 'true',
+     mode     => 700,
 
-  #   # TODO: Need to make it dependant on config as well
-  #   # require => Package['Pycharm'],
-  # }
+    # TODO: Need to make it dependant on config as well
+    require => Package['pycharm'],
 
+  }
 
+  file { "${pycharm_home}/templates":
+     ensure   => link,
+     target   => '/Users/indika/dev/config/pycharm/templates',
+     owner    => 'indika',
+     group    => 'staff',
+     force    => 'true',
+     mode     => 700,
+
+    # TODO: Need to make it dependant on config as well
+    require => Package['pycharm'],
+  }
 
 }
 
-# Need to recursively chown indika:admin
-
-# Now clone the keymap repo, depending on the packaging having been installed
-# /Users/indika/Library/Preferences/PyCharm40/keymaps
 
