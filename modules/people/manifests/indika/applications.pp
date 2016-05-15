9c3dcfa860f8c6d0f983c9893274abf87a8291d1
Indika Piyasena <indika@hydrasqr.com>
2016-05-15 14:13:38 +1000

    more cleanups

--- modules/people/manifests/indika/applications.pp
+++ modules/people/manifests/indika/applications.pp
@@ -2,7 +2,7 @@ class people::indika::applications {
 
   include brewcask
 
-  notice('I need to backup bettertouchtool plist. See mackup for help.')
+  package { 'airdroid': provider => 'brewcask' }
   package { 'bettertouchtool': provider => 'brewcask' }
   package { 'caffeine': provider => 'brewcask' }
   package { 'chicken': provider => 'brewcask' }
@@ -31,32 +31,8 @@ class people::indika::applications {
 
 
 
-  # include chrome
-  # include skype
-  # # include dropbox
-  # # include clipmenu
-  # include transmission
-  # # include java
-  # include vlc
-  # include spotify
-
-
-
-
-
-  # # class { 'sublime_text':
-  # #   build => '3083'
-  # # }
-
-
-  # include vlc
-  # include xquartz
-  # # include adobe_reader
-  # # include alfred
-#
   # include people::indika::applications::clamav
   include people::indika::applications::emacs
-  # #include people::indika::applications::fuse
   # include people::indika::applications::geektool
   include people::indika::applications::limechat
   include people::indika::applications::firefox
@@ -71,13 +47,8 @@ class people::indika::applications {
   # #TODO: Perhaps this should be a project
   # include people::indika::applications::squid
 
-  # # Handbrake is not mounting correctly
-  # # include handbrake
-
-  # # TODO: Can't get this to work
-  # # include brewcask
-  # # package { 'handbrakecli': provider => 'brewcask' }
-
+  # I do not use these anymore
+  # #include people::indika::applications::fuse
 
 
   # # include vagrant
@@ -90,6 +61,5 @@ class people::indika::applications {
   # include virtualbox
   # include firefox
   # include people::indika::applications::chicken_of_the_vnc
-
 
 }
